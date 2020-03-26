81b3fe4414d1097d52bbd50f3ca163d0497b0b2f
Samuele <62707286+Fancazista@users.noreply.github.com>
2020-03-26 20:01:41 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,20 +1,14 @@
 -----------=============================================[SCRIPT BY FANCAZISTA]=============================================-----------
 -- Script created by Fancazista, all rights reserved.
 
-dependency "vrp"
-
 server_scripts {
 	"@vrp/lib/utils.lua",
-
     "server.lua"
 }
 
 client_scripts {
 	"@vrp/client/Tunnel.lua",
 	"@vrp/client/Proxy.lua",
-    "lib/Tunnel.lua",
-    "lib/Proxy.lua",
-
     "client.lua"
 }
 
