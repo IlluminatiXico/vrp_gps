89e5863417adb362a9e89a781d2c90fb0cab7eaf
Samuele <62707286+Fancazista@users.noreply.github.com>
2020-03-26 20:02:20 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -6,6 +6,7 @@ local Tunnel = module("vrp", "lib/Tunnel")
 local Proxy = module("vrp", "lib/Proxy")
 
 vRP = Proxy.getInterface("vRP")
+vRPclient = Tunnel.getInterface("vRP", "vRP_rcCar")
 
 local stat = false
 
